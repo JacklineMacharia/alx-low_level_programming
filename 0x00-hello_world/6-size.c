adca72ce7a335ea54cd9e14e8f0ee97fd4c40dd6
JacklineMacharia <jacklinemacharia99@gmail.com>
2023-07-14 12:13:46 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - this is the main function
- * Return: 0 is always present
+ * main - This is the main function
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
