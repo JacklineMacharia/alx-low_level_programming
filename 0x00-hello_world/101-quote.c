e18bb7262fcfc4c1f3588ec61861a7dcae4ad5d2
JacklineMacharia <jacklinemacharia99@gmail.com>
2023-07-14 12:23:32 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
-/*main - code starts here
- *
- * Return: 1
- *
+
+/**
+ * main - prints exactly "and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19",
+ * followed by a new line, to the standard error.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
