39462cbc70118ff3b7069446baa7dd283529c2e5
JacklineMacharia <jacklinemacharia99@gmail.com>
2023-07-18 10:46:37 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * main - function that prints the sign of a number.
+ *
+ * Return: Is either 1 else -1 else 0.
+ *
+ */
+
+
+int print_sign(int n)
+
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+			return (0);
+	}
+}
