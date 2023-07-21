9e89391d26e0dd2190273485f1eec528de6c93f6
JacklineMacharia <jacklinemacharia99@gmail.com>
2023-07-20 23:53:14 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,15 +10,22 @@ void print_diagonal(int n)
 {
 	int i, j;
 
-	for (i = 0; i < n; i++)
+	if (n > 0)
 	{
-		if (n > 1)
+		for (i = 0; i < n; i++)
+		{
 			for (j = 0; j < i; j++)
 			{
 				_putchar(' ');
-			}
-		_putchar('\\');
-		_putchar('\n');
 
+			}
+			_putchar('\\');
+			_putchar('\n');
+
+		}
+	}
+	else
+	{
+		_putchar('\n');
 	}
 }
