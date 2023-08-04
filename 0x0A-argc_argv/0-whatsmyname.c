d48c7ddcdb0194fc755ace6d535720ba1a86427a
JacklineMacharia <jacklinemacharia99@gmail.com>
2023-08-04 13:09:32 -0700

    first quiz

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program
+ * @argc: number of arguments
+ * @argv: array of urguments
+ * Return:Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	(void)argc;
+	return (0);
+}
