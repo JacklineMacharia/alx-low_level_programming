ba932819be7bd3f7b7daccd65d07596205244db0
JacklineMacharia <jacklinemacharia99@gmail.com>
2023-08-15 02:27:43 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/main.h
+++ 0x0E-structures_typedef/main.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <unistd.h>
+#include "dog.h"
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
@@ -12,4 +13,4 @@ dog_t *new_dog(char *name, float age, char *owner);
 
 void free_dog(dog_t *d);
 
-#ifdef
+#endif
