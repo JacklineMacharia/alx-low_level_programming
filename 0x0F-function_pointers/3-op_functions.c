c697a1bcf5a771b05ede156550903d7205029aaf
JacklineMacharia <jacklinemacharia99@gmail.com>
2023-08-17 02:15:21 -0700

    A goal is not always meant to be reached, it often serves simply as something to aim at

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,53 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * op_add - 5 functions added
+ * @a: input one
+ * @b: input two
+ * Return: 0
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - 5 functions substraced
+ * @a: input one
+ * @b: input two
+ * Return: 0
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - 5 functions multiplied
+ * @a: input one
+ * @b: input two
+ * Return: 0
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - 5 functions divided
+ * @a: input one
+ * @b: input two
+ * Return: 0
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - 5 functions to find their modulus
+ * @a: input one
+ * @b: input two
+ * Return: 0
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
